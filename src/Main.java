fb8fb66c700f9eac173acd4c2e3af7aad0af0486
gopalbbb <gopal33001@gmail.com>
2022-12-12 21:00:14 -0500

    Hotel management System

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // print main page
+
+        System.out.println("Welcome To Extended Stay America");
+        System.out.println("Please choose your option");
+        System.out.println("1. House\t (In House Guest List)\t(Departures List)");
+        System.out.println("2.Reservation\t (CheckIn) ");
+        System.out.println("3.Admin Portal\t(Employees)\t(report)");
+
+
+        Scanner scan = new Scanner(System.in);
+       int choice=0;
+        // choose option
+        // implement option ( create method in another class
+        choice = scan.nextInt();
+        // when user choose number inside some funcation
+        if (choice == 1) {
+
+        } else if (choice == 2) {
+
+
+        } else if (choice == 3) {
+
+
+        }
+    }}
