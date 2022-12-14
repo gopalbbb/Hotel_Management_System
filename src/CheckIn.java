8545b016c8c0d2a18d27564a1c3567f1b61f13ce
gopalbbb <113260880+gopalbbb@users.noreply.github.com>
2022-12-13 22:16:53 -0500

    change new

---
+++ src/CheckIn.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class CheckIn {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String roomType;
+    private LocalDateTime checkInDate;
+
+
+}
