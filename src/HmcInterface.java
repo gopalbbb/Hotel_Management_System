4d71e60a9c692f5f81cae88247afc58a0d972b6d
puja <pujasigdel@yahoo.com>
2022-12-14 22:03:10 -0500

    add some classes

--- src/HmcInterface.java
+++ src/HmcInterface.java
@@ -1,5 +1,10 @@
 public interface  HmcInterface {
 
     public void checkIn();
-
+    public void checkOut();
+    public void setNumberOfRooms();
+    public void makeReservation();
+    public void cancelReservation();
+    public void getAllReservationSortedByPrice();
+   // getAllReservationSortedByDate();
 }
